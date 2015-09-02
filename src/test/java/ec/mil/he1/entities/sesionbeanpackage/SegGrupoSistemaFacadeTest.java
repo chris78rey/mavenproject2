57575abaf5ab37d7daf0e26eb2dc7231ba4f42b8
chris78rey <cris_78_rey@hotmail.com>
2015-09-02 11:49:45 -0500

    se va a implementar los cambios del POM DE junit, este se puede utilizar para documentar

--- src/test/java/ec/mil/he1/entities/sesionbeanpackage/SegGrupoSistemaFacadeTest.java
+++ src/test/java/ec/mil/he1/entities/sesionbeanpackage/SegGrupoSistemaFacadeTest.java
@@ -8,8 +8,10 @@ package ec.mil.he1.entities.sesionbeanpackage;
 import ec.mil.he1.entidades.SegGrupoSistema;
 import java.util.List;
 import javax.ejb.embeddable.EJBContainer;
-import org.junit.Test;
+import org.junit.Assert;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  *
@@ -19,83 +21,84 @@ public class SegGrupoSistemaFacadeTest {
     
     public SegGrupoSistemaFacadeTest() {
     }
-
-    @org.junit.Test
+    
+    @Ignore
     public void testCreate() throws Exception {
         System.out.println("create");
         SegGrupoSistema entity = null;
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade)container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
+        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade) container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
         instance.create(entity);
         container.close();
         fail("The test case is a prototype.");
     }
-
-    @org.junit.Test
+    
+    @Ignore
     public void testEdit() throws Exception {
         System.out.println("edit");
         SegGrupoSistema entity = null;
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade)container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
+        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade) container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
         instance.edit(entity);
         container.close();
         fail("The test case is a prototype.");
     }
-
-    @org.junit.Test
+    
+    @Ignore
     public void testRemove() throws Exception {
         System.out.println("remove");
         SegGrupoSistema entity = null;
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade)container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
+        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade) container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
         instance.remove(entity);
         container.close();
         fail("The test case is a prototype.");
     }
-
-    @org.junit.Test
+    
+    @Ignore
     public void testFind() throws Exception {
         System.out.println("find");
         Object id = null;
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade)container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
+        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade) container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
         SegGrupoSistema expResult = null;
         SegGrupoSistema result = instance.find(id);
         assertEquals(expResult, result);
         container.close();
         fail("The test case is a prototype.");
     }
-
-    @org.junit.Test
+    
+    @Test
     public void testFindAll() throws Exception {
         System.out.println("findAll");
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade)container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
+        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade) container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
         List<SegGrupoSistema> expResult = null;
         List<SegGrupoSistema> result = instance.findAll();
         assertEquals(expResult, result);
+        Assert.assertTrue("SE ejecuta correctamente", true);
         container.close();
         fail("The test case is a prototype.");
     }
-
-    @org.junit.Test
+    
+    @Ignore
     public void testFindRange() throws Exception {
         System.out.println("findRange");
         int[] range = null;
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade)container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
+        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade) container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
         List<SegGrupoSistema> expResult = null;
         List<SegGrupoSistema> result = instance.findRange(range);
         assertEquals(expResult, result);
         container.close();
         fail("The test case is a prototype.");
     }
-
-    @org.junit.Test
+    
+    @Ignore
     public void testCount() throws Exception {
         System.out.println("count");
         EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade)container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
+        SegGrupoSistemaFacade instance = (SegGrupoSistemaFacade) container.getContext().lookup("java:global/classes/SegGrupoSistemaFacade");
         int expResult = 0;
         int result = instance.count();
         assertEquals(expResult, result);
